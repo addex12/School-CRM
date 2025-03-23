c7a42fea607f6307ed7f7e39767b1b6b8161cd9d
Adugna <gizawadugna@gmail.com>
2025-03-23 18:32:22 +0300

    Work around with some other files

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <!--Author: Adugna Gizaw
 Email: gizawadugna@gmail.com
-Phone: +251925582067 --!>
+Phone: +251925582067 -->
 <?php
 $message = '';
 
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 }
-
+?>
 
 <!DOCTYPE html>
 <html lang="en">
