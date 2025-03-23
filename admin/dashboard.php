328b25e0ef1fdd2c34dfa1963044ec6aec3825ac
Adugna <gizawadugna@gmail.com>
2025-03-23 20:45:36 +0300

    Work around on install.php

---
+++ admin/dashboard.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Admin Dashboard
+ * 
+ * This script displays the admin dashboard.
+ * 
+ * Developer: Adugna Gizaw
+ * Email: gizawadugna@gmail.com
+ * Phone: +251925582067
+ * GitHub: https://github.com/addex12
+ * LinkedIn: https://www.linkedin.com/in/eleganceict
+ * Twitter: https://twitter.com/eleganceict1
+ */
+
+// ...existing code...
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Admin Dashboard</h1>
+        <p>Welcome to the admin dashboard.</p>
+        <!-- ...existing code... -->
+    </div>
+</body>
+</html>
