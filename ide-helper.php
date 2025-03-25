f89e3dd7cf9ded77e26a160deccb425b18722654
Adugna Gizaw <gizawadugna@gmail.com>
2025-03-26 00:46:07 +0300

    Delete ide-helper.php

--- ide-helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Spreadsheet.php';
-require_once __DIR__ . '/vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/Xlsx.php';
-
-// Fake declarations for IDE
-if (false) {
-    class \PhpOffice\PhpSpreadsheet\Spreadsheet {}
-    class \PhpOffice\PhpSpreadsheet\Writer\Xlsx {}
-}
