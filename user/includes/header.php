62b560cbcaeab55cdcbab93ea6aea5b079861c01
Adugna <gizawadugna@gmail.com>
2025-03-27 00:54:40 +0300

    fix: update header links to correct profile navigation and remove inbox link

--- user/includes/header.php
+++ user/includes/header.php
@@ -29,11 +29,8 @@ requireLogin();
                 <a href="contact.php" class="<?= basename($_SERVER['PHP_SELF']) === 'contact.php' ? 'active' : '' ?>">
                     <i class="fas fa-envelope"></i> Contact
                 </a>
-                <a href="account.php" class="<?= basename($_SERVER['PHP_SELF']) === 'profile.php' ? 'active' : '' ?>">
+                <a href="profile.php" class="<?= basename($_SERVER['PHP_SELF']) === 'profile.php' ? 'active' : '' ?>">
                     <i class="fas fa-user"></i> Account
-                </a>
-                <a href="../inbox.php" class="logout">
-                    <i class="fas fa-sign-out-alt"></i> Inbox
                 </a>
                 <a href="../logout.php" class="logout">
                     <i class="fas fa-sign-out-alt"></i> Logout
