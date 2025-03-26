2cbf6953b6a42020b3d06bbe47818971bd0f00d4
Adugna <gizawadugna@gmail.com>
2025-03-26 04:05:10 +0300

    feat: include header file in admin and user modules for consistency

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . 'includes/header.php';
 require_once '../includes/config.php'; // Include config to initialize $pdo
 require_once '../includes/auth.php';
 requireAdmin();
