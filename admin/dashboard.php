0051dffdf6425024231925832411d62623558032
Adugna <gizawadugna@gmail.com>
2025-03-26 07:26:35 +0300

    fix: update inclusion paths in dashboard.php and restructure header.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,7 +2,7 @@
 // Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include '../includes/header.php';
+include 'includes/header.php';
 require_once '../includes/auth.php';
 requireAdmin();
 require_once '../includes/config.php'; // Include config to initialize $pdo
