32f9d17f83ee8692e142cc6b5f4310ca3c394722
Adugna <gizawadugna@gmail.com>
2025-03-26 08:16:44 +0300

    fix: add dynamic primary color styling in header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -80,7 +80,11 @@ if (empty($adminMenu)) {
     <title><?= htmlspecialchars($siteName) ?> - Admin | <?= htmlspecialchars($pageTitle ?? 'Dashboard') ?></title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-   
+    <style>
+        :root {
+            --primary-color: <?= htmlspecialchars($themeColor) ?>;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
