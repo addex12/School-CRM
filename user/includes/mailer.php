3a8a686eeb8cca45d6decf8bebc8a4688c77e7a2
Adugna <gizawadugna@gmail.com>
2025-03-26 05:02:12 +0300

    fix: correct include path formatting for PHPMailer classes in mailer.php

--- user/includes/mailer.php
+++ user/includes/mailer.php
@@ -1,8 +1,8 @@
 <?php
 // Load PHPMailer classes
-require_once __DIR__ . '/../PHPMailer/Exception.php';
-require_once __DIR__ . '/../PHPMailer/PHPMailer.php';
-require_once __DIR__ . '/../PHPMailer/SMTP.php';
+require_once '../PHPMailer/Exception.php';
+require_once '../PHPMailer/PHPMailer.php';
+require_once '../PHPMailer/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
