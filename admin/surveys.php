f64d1cd2e71a780155ba093211939ddb1fd3f93e
Adugna <gizawadugna@gmail.com>
2025-03-28 00:23:16 +0300

    Fix formatting of results link in admin surveys page for improved readability

--- admin/surveys.php
+++ admin/surveys.php
@@ -83,8 +83,7 @@ $surveys = $pdo->query("SELECT * FROM surveys ORDER BY created_at DESC")->fetchA
                                         </td>
                                         <td>
                                         <a href="survey_preview.php?id=<?= $survey['id'] ?>" class="btn btn-primary">Preview</a>
-                                            <a href="results.php?survey_id=<?= $survey['id'] ?>" class="btn btn-secondary">Results</a>
-                                        </td>
+                                        <a href="results.php?survey_id=<?= $survey['id'] ?>" class="btn btn-secondary">Results</a>                                        </td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
