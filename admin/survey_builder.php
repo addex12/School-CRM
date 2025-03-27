14c16eeb7fb847c26513260b57fdd38a53467883
Adugna Gizaw <gizawadugna@gmail.com>
2025-03-27 12:14:13 +0300

    update

--- admin/survey_builder.php
+++ admin/survey_builder.php
@@ -125,7 +125,7 @@ $categories = $pdo->query("SELECT * FROM survey_categories ORDER BY name")->fetc
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $survey_id ? 'Edit Survey' : 'Create New Survey'; ?> - Admin Panel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="survey_builder.css">
 </head>
 <body>
     <div class="admin-container">
