bad94ab4cf3a0eece9aa58db16dafea43656e1fa
Adugna <gizawadugna@gmail.com>
2025-03-28 21:42:30 +0300

    Update login page title and heading to reflect School CRM branding

--- login.php
+++ login.php
@@ -80,7 +80,7 @@ if (isset($_GET['provider'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Survey System</title>
+    <title>Login - School CRM</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
@@ -147,7 +147,7 @@ if (isset($_GET['provider'])) {
         <div class="login-logo">
             <i class="fas fa-poll-h"></i>
         </div>
-        <h1 class="login-title">Survey System Login</h1>
+        <h1 class="login-title">School CRM System Login</h1>
         
         <?php if ($error): ?>
             <div class="error-message"><?php echo $error; ?></div>
