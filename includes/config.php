8f93aadc8ddcce98a4c56a957abac492422efbfe
Adugna <gizawadugna@gmail.com>
2025-03-29 22:32:45 +0300

    Update config.php to include adminsetting.php for getSystemSetting definition

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once __DIR__ . '/../vendor/autoload.php'; // Include Composer autoloader
 require_once 'db.php';
-require_once 'setting.php'; // Include the file that defines getSystemSetting
+require_once '../adminsetting.php'; // Include the file that defines getSystemSetting
 
 // Database connection
 $database = new Database();
