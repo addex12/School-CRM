b17ab92b7e0c51aa542cb3e4e9c196eaec960dd6
Adugna <gizawadugna@gmail.com>
2025-03-29 15:36:24 +0300

    Refactor register.php: update database connection file reference

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
 
 require_once 'includes/config.php';
 require_once 'includes/auth.php';
-require_once 'includes/Database.php';
+require_once 'includes/db.php';
 
 // Initialize database connection
 $db = new Database();
