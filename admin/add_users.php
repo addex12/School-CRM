5954dafbebb58d88a2c18f7dcedf744f2b7db2d1
Adugna <gizawadugna@gmail.com>
2025-03-29 14:56:23 +0300

    Refactor add_users.php: update email notifications to include personalized welcome messages and correct sender address

--- admin/add_users.php
+++ admin/add_users.php
@@ -85,8 +85,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     // Send email (optional)
                     $to = $email;
                     $subject = "Your New Account";
-                    $message = "Username: $username\nTemporary Password: $temp_password";
-                    $headers = "From: no-reply@example.com";
+                    $message = "Welcome To School CRM! Your Account has been created successfully! \n Following are your new credentioals\nUsername: $username\nTemporary Password: $temp_password";
+                    $headers = "From: adugna.gizaw@flipperschools.com";
                     @mail($to, $subject, $message, $headers);
                 }
 
@@ -143,7 +143,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Send email (optional)
             $to = $email;
             $subject = "Your New Account";
-            $message = "Username: $username\nTemporary Password: $temp_password";
+            $message = "Dear School CRM Family! As per your request, following are your temporary credentials.\nUsername: $username\nTemporary Password: $temp_password";
             $headers = "From: no-reply@example.com";
             @mail($to, $subject, $message, $headers);
 
