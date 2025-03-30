c12a00bcbc08719360b555ec5aac407419a5a068
Adugna <gizawadugna@gmail.com>
2025-03-30 12:03:59 +0300

    Fix footer include path in profile file to ensure correct file loading

--- user/profile.php
+++ user/profile.php
@@ -237,6 +237,6 @@ include __DIR__ . '/../includes/header.php';
     </div>
 </div>
 
-<?php include __DIR__ . '/../includes/footer.php'; 
+<?php include __DIR__ . '/includes/footer.php'; 
 ob_end_flush();
 ?>
