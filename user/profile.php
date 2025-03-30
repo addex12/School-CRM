e5b177d4cb828b12e42b53822585aa11e3b97972
Adugna <gizawadugna@gmail.com>
2025-03-31 00:57:34 +0300

    Add footer inclusion and scripts to profile page for consistent layout

--- user/profile.php
+++ user/profile.php
@@ -257,9 +257,15 @@ function sendPasswordChangeNotification($email) {
             </div>
         </div>
     </div>
-</div>
+    <?php include_once __DIR__ . '/includes/footer.php';
+    ?>  </div>
+<?php
+ob_end_flush(); ?>
 
-<?php include_once __DIR__ . '/includes/footer.php';
-ob_end_flush();
-?>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="js/profile.js"></script>
+</body> </html>
