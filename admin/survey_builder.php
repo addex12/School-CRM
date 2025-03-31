d6c561907de6798d86016df13fe347b4f4c94f1c
Adugna <gizawadugna@gmail.com>
2025-04-01 00:04:35 +0300

    Remove dynamic title from survey builder for improved consistency

--- admin/survey_builder.php
+++ admin/survey_builder.php
@@ -140,7 +140,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $survey_id ? 'Edit Survey' : 'New Survey' ?> - Survey Builder</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="survey_builder.css">
 </head>
