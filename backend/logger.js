d501e3b174110a85a409d44c37ff0264cc74e3b1
Adugna Gizaw <gizawadugna@gmail.com>
2025-04-03 10:07:24 +0300

    update

--- backend/logger.js
+++ backend/logger.js
@@ -1,23 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-
-const logFile = path.join(__dirname, 'logs.txt');
-
-module.exports = {
-    log(type, data) {
-        const timestamp = new Date().toISOString();
-        const logEntry = `[${timestamp}] [${type}] ${JSON.stringify(data)}\n`;
-        fs.appendFileSync(logFile, logEntry, 'utf8');
-    },
-};
 const { createLogger, format, transports } = require('winston');
 const path = require('path');
 const fs = require('fs');
+
 const logDir = path.join(__dirname, 'logs');
+
 // Create log directory if it doesn't exist
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir);
 }
+
 // Create a logger instance
 const logger = createLogger({
     level: 'info',
@@ -37,13 +28,11 @@ const logger = createLogger({
         })
     ]
 });
-module.exports = [
-    logger,
-    {
-        log(type, data) {
-            logger.info(`[${type}] ${JSON.stringify(data)}`);
-        },
-    },
-]
-// Export the logger
-module.exports = logger;
+
+// Function to log messages
+function log(type, data) {
+    logger.log(type, JSON.stringify(data));
+}
+
+// Export the logger and log function
+module.exports = { logger, log };
