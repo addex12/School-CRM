c0006ab2d0009bb95e142a7cf2bfcadceacdf8e7
Adugna <gizawadugna@gmail.com>
2025-04-21 22:12:08 +0300

    Fix include path for config file in feedback page

--- user/feedback.php
+++ user/feedback.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/config.php';
 require_once __DIR__ . '/../includes/db.php';
 require_once 'includes/mailer.php';
 requireLogin();
