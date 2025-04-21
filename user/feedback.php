5eb72d642131010027c164449731f97e63507e7b
Adugna <gizawadugna@gmail.com>
2025-04-21 22:06:54 +0300

    Fix include paths for required files in feedback page

--- user/feedback.php
+++ user/feedback.php
@@ -1,7 +1,7 @@
 <?php
-require_once '/../includes/auth.php';
-require_once '/../includes/db.php';
-require_once '/includes/mailer.php';
+require_once '../includes/auth.php';
+require_once '../includes/db.php';
+require_once '../includes/mailer.php';
 require_once '../includes/config.php';
 requireLogin();
 
