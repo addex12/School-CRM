424019be907011ecbb050f1959aa6015eab6c70d
Adugna <gizawadugna@gmail.com>
2025-04-21 19:47:44 +0300

    Fix missing slash in auth.php require statement in register.php

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/functions.php';
 require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/includes/auth.php';
 
 $pageTitle = 'Register';
 
