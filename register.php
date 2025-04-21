383e260d6a4df7cb703761809666198ca50b4d31
Adugna <gizawadugna@gmail.com>
2025-04-21 19:58:50 +0300

    Remove unnecessary auth.php inclusion from register.php

--- register.php
+++ register.php
@@ -2,7 +2,6 @@
 require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/functions.php';
 require_once __DIR__ . '/includes/header.php';
-require_once __DIR__ . '/includes/auth.php';
 
 $pageTitle = 'Register';
 
