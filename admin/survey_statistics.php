ee89fc7ef4bf7fffa9e0c5ef20038557952eb255
Adugna <gizawadugna@gmail.com>
2025-04-22 02:46:16 +0300

    Add developer information to survey statistics file

--- admin/survey_statistics.php
+++ admin/survey_statistics.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Developer: Adugna Gizaw
+ * Email: gizawadugna@gmail.com
+ * LinkedIn: https://www.linkedin.com/in/eleganceict
+ * Twitter: https://twitter.com/eleganceict1
+ * GitHub: https://github.com/addex12
+ */
 ob_start();
 require_once '../includes/auth.php';
 requireAdmin();
