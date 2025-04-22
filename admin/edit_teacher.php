a7e6fc8edea04eb6ab09349373677a3837d01e28
Adugna <gizawadugna@gmail.com>
2025-04-23 00:13:52 +0300

    Refactor edit_teacher.php: correct section ID reference in teacher subjects query

--- admin/edit_teacher.php
+++ admin/edit_teacher.php
@@ -45,13 +45,13 @@ $teacher_subjects_stmt = $pdo->prepare("
         cs.class_id,
         s.subject_name,
         c.class_name,
-        ts.section_id,
+        ts.section,
         sec.section_name
     FROM teacher_subjects ts
     JOIN class_subjects cs ON ts.class_subject_id = cs.id
     JOIN subjects s ON cs.subject_id = s.id
     LEFT JOIN classes c ON cs.class_id = c.id
-    LEFT JOIN sections sec ON ts.section_id = sec.id
+    LEFT JOIN sections sec ON ts.section = sec.id
     WHERE ts.teacher_id = ?
     ORDER BY c.class_name, sec.section_name, s.subject_name
 ");
