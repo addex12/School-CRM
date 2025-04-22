9253f25ddb2e2fc9707d2189a834be1e955c2db1
Adugna <gizawadugna@gmail.com>
2025-04-23 02:29:15 +0300

    Fix indentation for footer inclusion in grade reports page

--- admin/grades.php
+++ admin/grades.php
@@ -348,7 +348,8 @@ $grades = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </div>
             </div>
         </div>
-        <?php include 'includes/footer.php'; ?>
     </div>
+            <?php include 'includes/footer.php'; ?>
+
 </body>
 </html>
