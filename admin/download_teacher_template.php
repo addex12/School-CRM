e50f5003850973a434f7350310185d01b0e752b6
Adugna <gizawadugna@gmail.com>
2025-04-22 20:05:17 +0300

    Add teacher import functionality with CSV template and data handling

---
+++ admin/download_teacher_template.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="teacher_import_template.csv"');
+
+$columns = [
+    'username',
+    'email',
+    'phone',
+    'address',
+    'date_of_birth',
+    'gender',
+    'qualification',
+    'subject_specialization',
+    'status',
+    'class_name'
+];
+
+$output = fopen('php://output', 'w');
+fputcsv($output, $columns);
+fclose($output);
+exit;
