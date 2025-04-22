26db623de0594b4e1fd82c9dfe2f67f6068dcc8e
Adugna <gizawadugna@gmail.com>
2025-04-23 00:54:31 +0300

    Refactor get_subjects.php: simplify subject retrieval logic by removing unnecessary joins and fields

--- admin/ajax/get_subjects.php
+++ admin/ajax/get_subjects.php
@@ -12,22 +12,11 @@ if (!isset($_GET['class_id'])) {
 $class_id = intval($_GET['class_id']);
 
 $stmt = $pdo->prepare("
-    ts.id,
-    t.id AS teacher_id,
-    cs.id AS class_subject_id,
-    c.id AS class_id,
-    c.class_name,
-    s.id AS subject_id,
-    s.subject_name,
-    sec.id AS section_id,
-    sec.section_name
-FROM teacher_subjects ts
-JOIN teachers t ON ts.teacher_id = t.id
-JOIN class_subjects cs ON ts.class_subject_id = cs.id
-JOIN classes c ON cs.class_id = c.id
-JOIN subjects s ON cs.subject_id = s.id
-LEFT JOIN sections sec ON ts.section_id = sec.id
-WHERE ts.teacher_id = ?;
+    SELECT s.id, s.subject_name
+    FROM class_subjects cs
+    JOIN subjects s ON cs.subject_id = s.id
+    WHERE cs.class_id = ?
+    ORDER BY s.subject_name
 ");
 $stmt->execute([$class_id]);
 $subjects = $stmt->fetchAll(PDO::FETCH_ASSOC);
