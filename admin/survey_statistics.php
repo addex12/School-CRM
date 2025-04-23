1b94910ee2c33df2f119024a4664fcfdeb17335f
Adugna <gizawadugna@gmail.com>
2025-04-23 21:57:53 +0300

    Fix title rendering in survey statistics page; use dynamic page title variable

--- admin/survey_statistics.php
+++ admin/survey_statistics.php
@@ -94,7 +94,7 @@ $chart_json = json_encode($chart_data);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Survey Statistics - Admin Panel</title>
+    <title><?= esc($pageTitle) ?> - Admin Panel</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
