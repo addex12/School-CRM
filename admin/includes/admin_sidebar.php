c062b61d91e2059a7b65c07904a9fef9422528a8
Adugna <gizawadugna@gmail.com>
2025-04-24 17:51:52 +0300

    Fix sidebar collapsed width: update width to 90px

--- admin/includes/admin_sidebar.php
+++ admin/includes/admin_sidebar.php
@@ -30,7 +30,7 @@ if (file_exists($configPath)) {
     flex-direction: column;
 }
 .admin-sidebar.collapsed {
-    width: 75px;
+    width: 90px;
 }
 .admin-sidebar .sidebar-header {
     padding: 1.2rem 1.5rem;
