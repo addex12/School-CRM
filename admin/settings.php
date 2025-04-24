ef032d39a98c35c4f81bc4eac7d22dbc0c6a3d15
Adugna <gizawadugna@gmail.com>
2025-04-24 16:14:30 +0300

    Add developer information and fix footer inclusion formatting in settings page

--- admin/settings.php
+++ admin/settings.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Developer: Adugna Gizaw
+ * Email: gizawadugna@gmail.com
+ * LinkedIn: https://www.linkedin.com/in/eleganceict
+ * Twitter: https://twitter.com/eleganceict1
+ * GitHub: https://github.com/addex12
+ */
 require_once '../includes/auth.php';
 requireAdmin();
 require_once '../includes/config.php';
@@ -162,7 +169,8 @@ $settings_fields = [
                 </ul>
             </div>
         </div>
-        <?php include 'includes/footer.php'; ?>
     </div>
+            <?php include 'includes/footer.php'; ?>
+
 </body>
 </html>
