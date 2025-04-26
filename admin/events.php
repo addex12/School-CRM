f34f32105d475c28a79985f69c49a822229062e2
Adugna <gizawadugna@gmail.com>
2025-04-27 02:32:12 +0300

    fix: remove unnecessary auth include and correct table cell structure in events.php

--- admin/events.php
+++ admin/events.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/auth.php';
+
 
 if (!Auth::isLoggedIn()) {
     header("Location: ../login.php");
@@ -44,7 +44,7 @@ include 'includes/header.php';
                         <td><?= htmlspecialchars($event['title']) ?></td>
                         <td><?= date('M j, Y g:i A', strtotime($event['start_date'])) ?></td>
                         <td><?= date('M j, Y g:i A', strtotime($event['end_date'])) ?></td>
-                        <td></td>
+                        <td>
                             <a href="event-edit.php?id=<?= $event['id'] ?>">Edit</a>
                             <a href="event-delete.php?id=<?= $event['id'] ?>" onclick="return confirm('Are you sure you want to delete this event?');">Delete</a>
                         </td>
