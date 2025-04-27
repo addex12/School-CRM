3056374e4c2d1860e2484591df11328651fb700f
Adugna <gizawadugna@gmail.com>
2025-04-27 11:07:37 +0300

    style: add missing CSS links for improved styling in add_users.php

--- admin/add_users.php
+++ admin/add_users.php
@@ -267,6 +267,9 @@ $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
     <title><?= htmlspecialchars($pageTitle) ?> - Admin Panel</title>
     <link rel="stylesheet" href="../assets/css/add_users.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/admin.css">
+    <link rel="stylesheet" href="../assets/css/erpnext.css">
+    <link rel="stylesheet" href="../assets/css/erpnext_form.css">
 
     <style>
         .erpnext-form-grid {
