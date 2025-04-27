b3a5112bba01e2c6617dbd0d80606d8425e98656
Adugna <gizawadugna@gmail.com>
2025-04-27 03:18:41 +0300

    fix: always save uploaded site logo as 'logo.png' for consistency

--- admin/settings.php
+++ admin/settings.php
@@ -21,8 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_settings'])) {
             if (!is_dir($uploadDir)) {
                 mkdir($uploadDir, 0777, true);
             }
-            $fileName = basename($_FILES['site_logo']['name']);
-            $targetFile = $uploadDir . $fileName;
+            $targetFile = $uploadDir . 'logo.png'; // Always save as logo.png
             if (move_uploaded_file($_FILES['site_logo']['tmp_name'], $targetFile)) {
                 $_POST['settings']['site_logo'] = $targetFile;
             } else {
