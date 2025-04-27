334a2bdc51530962d98893586a2112004ab756d5
Adugna <gizawadugna@gmail.com>
2025-04-27 15:48:17 +0300

    style: remove unused Font Awesome CSS files to streamline dashboard styling

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -199,9 +199,6 @@ try {
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/fontawesome.min.css">
-    <link rel="stylesheet" href="../assets/css/brands.min.css">
-    <link rel="stylesheet" href="../assets/css/solid.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="../assets/js/dashboard.js" defer></script>
     <style>
