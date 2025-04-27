9f80a84a997bfc630143bde501bd73746c10d05f
Adugna <gizawadugna@gmail.com>
2025-04-27 11:06:07 +0300

    style: remove unused CSS links for cleaner markup in add_users.php

--- admin/add_users.php
+++ admin/add_users.php
@@ -265,8 +265,6 @@ $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?> - Admin Panel</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="../assets/css/add_users.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
