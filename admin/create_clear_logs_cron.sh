16994df0bc97a4eb9874e16879e4742c643a7646
Adugna <gizawadugna@gmail.com>
2025-04-29 21:22:29 +0300

    fix: remove unnecessary blank line in cron job script

--- admin/create_clear_logs_cron.sh
+++ admin/create_clear_logs_cron.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 CRON_JOB="0 2 * * * curl -fsS https://crm.flipperschool.com/admin/clear_logs.php >/dev/null 2>&1"
 CRON_EXISTS=$(crontab -l 2>/dev/null | grep -F "$CRON_JOB")
 # Check if the cron job already exists
