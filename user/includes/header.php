eb2584e77af663134af30365c1d9fe4c3068a7fd
Adugna <gizawadugna@gmail.com>
2025-05-01 13:44:41 +0300

    fix: update link to announcements page for consistency

--- user/includes/header.php
+++ user/includes/header.php
@@ -342,7 +342,7 @@ if (isset($_SESSION['user_id'])) {
                             New Messages
                             <span class="notification-count"><?= $newMessageCount ?></span>
                         </a>
-                        <a href="announcements.php">
+                        <a href="announcement.php">
                             <i class="fas fa-bullhorn"></i>
                             New Announcements
                             <span class="notification-count"><?= $newAnnouncementCount ?></span>
