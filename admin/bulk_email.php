f72ea7fd3f27a4af7fbd4eeff67fc0c899206cc0
Adugna <gizawadugna@gmail.com>
2025-05-08 19:41:31 +0300

    fix: reorder requireAdmin() call for proper session management

--- admin/bulk_email.php
+++ admin/bulk_email.php
@@ -7,8 +7,8 @@ Twitter: https://twitter.com/eleganceict1
 GitHub: https://github.com/addex12
 */
 require_once '../includes/auth.php';
-requireAdmin();
 require_once '../includes/config.php';
+requireAdmin();
 
 // Handle form submissions
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
