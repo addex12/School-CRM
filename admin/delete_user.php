6e4511549041a1ce681cba33125156b73bed9dad
Adugna <gizawadugna@gmail.com>
2025-05-10 02:53:50 +0300

    Remove unnecessary logging requirement in delete_user.php

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -5,7 +5,6 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once '../includes/config.php';
 require_once '../includes/auth.php';
 requireAdmin();
-require_once 'system_logs.php';
 
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     $_SESSION['error'] = "Invalid user ID.";
