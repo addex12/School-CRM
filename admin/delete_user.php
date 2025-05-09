512e4a911b0cbf350913aff1cf132ff3aa42071b
Adugna <gizawadugna@gmail.com>
2025-05-10 02:46:41 +0300

    Remove logging of user deletion action in delete_user.php

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -36,7 +36,6 @@ if (!$user) {
 $stmt = $pdo->prepare("DELETE FROM users WHERE id = ?");
 $stmt->execute([$id]);
 
-adugna_log_system_action($pdo, 'Delete User', "Deleted user ID $id", $_SESSION['user_id']);
 
 $_SESSION['success'] = "User deleted successfully.";
 header("Location: users.php");
